8be94e9fe8517ac0aa44431864965f74b3cb502f
nourhan36 <nourhanibrahim2004@gmail.com>
2024-02-02 18:39:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/todo/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.todo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(mainLooper).postDelayed({
+            startHomeActivity()
+        },2000)
+    }
+
+    private fun startHomeActivity() {
+        val intent = Intent(this,HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
