3f00b53a845124c318cb43ac38e6afac643d92ed
nourhan36 <nourhanibrahim2004@gmail.com>
2024-02-04 20:59:58 +0200

    + fragment tasks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //calendar
+    implementation("com.github.prolificinteractive:material-calendarview:2.0.1") {
+        exclude(group = "com.android.support", module = "support-compat")
+    }
+
 }
