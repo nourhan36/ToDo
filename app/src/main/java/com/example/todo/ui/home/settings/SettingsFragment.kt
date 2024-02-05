66095e03dff66ba32d73071d6d8458ee3e572530
nourhan36 <nourhanibrahim2004@gmail.com>
2024-02-05 18:18:57 +0200

    + fragment settings

--- app/src/main/java/com/example/todo/ui/home/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/todo/ui/home/settings/SettingsFragment.kt
@@ -4,17 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.todo.R
 import com.example.todo.databinding.FragmentSettingsBinding
 
 class SettingsFragment: Fragment() {
     lateinit var viewBinding: FragmentSettingsBinding
-    var languageItem = arrayListOf("English" , "Arabic")
-    var modeItem = arrayListOf("Light" , "Dark")
-    lateinit var autoCompleteTextView: AutoCompleteTextView
-    lateinit var adapterItems: ArrayAdapter<String>
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +28,37 @@ class SettingsFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
 
+        val languageItem = arrayListOf("English", "Arabic")
+        val modeItem = listOf("Light", "Dark")
+
+        val autoCompleteLanguage: AutoCompleteTextView = viewBinding.languageTv
+        val autoCompleteMode: AutoCompleteTextView = viewBinding.modeTv
+
+        val adapterLanguage = ArrayAdapter(requireContext(), R.layout.list_item, languageItem)
+        val adapterMode = ArrayAdapter(requireContext(), R.layout.list_item, modeItem)
+
+        autoCompleteLanguage.setAdapter(adapterLanguage)
+        autoCompleteMode.setAdapter(adapterMode)
+
+        autoCompleteLanguage.setOnItemClickListener { adapterView, view, position, id ->
+            val selectedText = adapterView.getItemAtPosition(position)
+        }
+
+        autoCompleteMode.setOnItemClickListener { adapterView, view, position, id ->
+            val selectedText = adapterView.getItemAtPosition(position)
+            if (selectedText == "Dark") {
+                viewBinding.modeTil.startIconDrawable = ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.ic_dark_mode
+                )
+            } else {
+                // Set the default drawable for other modes
+                viewBinding.modeTil.startIconDrawable = ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.ic_light_mode
+                )
+            }
+        }
+    }
 }
